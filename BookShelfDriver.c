4ab4f404dfec4a3b9797a7c03cff06c268dbda88
Zenny <joshuacomfort@gmail.com>
2023-06-21 08:12:08 -0400

    Added function to create a new book node given predefined values

--- BookShelfDriver.c
+++ BookShelfDriver.c
@@ -5,9 +5,17 @@
 #include "LinkedList.h"
 
 int main(int argc, char **argv) {
+    // Create book from keyboard input
     struct book *book_node = create_node();
     print_node(book_node);
     destroy_node(book_node);
+
+    // Create book given predefined values
+    struct book *book_node_1 = create_node_given_data("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 1981);
+    print_node(book_node_1);
+    destroy_node(book_node_1);
+
+    // Debug log
     printf("hi\n");
     return 0;
 }
--- LinkedList.h
+++ LinkedList.h
@@ -28,7 +28,6 @@ struct book {
 };
 
 // Create node function one function given the input parameters the other will prompt the user for input from the keyboard
-
 struct book * create_node() {
     struct book *new_book = (struct book*)malloc(sizeof(struct book)); // Create a new book on the heap
 
@@ -89,6 +88,39 @@ struct book * create_node() {
     return new_book;
 }
 
+struct book * create_node_given_data(char *name, char *author, int date) {
+    struct book *new_book = (struct book*)malloc(sizeof(struct book)); // Create a new book on the heap
+
+    // Guard clause for full heap
+    if (new_book == NULL) {
+        // Heap is full
+        printf("The heap is full, no new space to allocate.\n");
+        exit(0);
+    }
+
+    // Allocate space for input from console
+    // 1 is added to the maximum to leave space for the end of line character '\0'
+    new_book->name = (char *)malloc((MAX_AUTHOR_NAME_LENGTH+1)*sizeof(char)); 
+    new_book->author = (char *)malloc((MAX_BOOK_TITLE_LENGTH+1)*sizeof(char));
+
+    // If heap is full, exit the program
+    if (new_book->name == NULL || new_book->author == NULL) {
+        printf("The heap is full, no new space to allocate.\n");
+        exit(0);
+    }
+
+    // Assign values based on input parameters
+    strcpy(new_book->name, name);
+    strcpy(new_book->author, author);
+    new_book->date = date;
+
+    // Next book is initially set to nothing
+    new_book->next = NULL;
+
+    // Return the reference to the newly created node
+    return new_book;
+}
+
 // Given a pointer to a book node, first free the memory allocated to store the data attributes then free the memory allocated to store the struct
 void destroy_node(struct book *book_node) {
     free(book_node->name);
@@ -101,6 +133,12 @@ void print_node(struct book *book_node) {
     printf("The name of the book is %s\n", book_node->name);
     printf("The name of the author is %s\n", book_node->author);
     printf("The date of publication is %d\n", book_node->date);
+}
+
+// Add a node to the linked list, books are added in order of the titles, sorted in alphabetical order
+// Takes in a reference to the head of the list as an input parameter
+void insert(struct book *head) {
+    return;
 }
 
 #endif
